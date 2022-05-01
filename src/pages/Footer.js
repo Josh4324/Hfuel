91eb275eb87efc65669a0d56124941effd139476
Josh <adesanyajoshua@ymail.com>
2022-05-01 22:46:58 +0100

    update

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div>
-      <div className="text4" style={{ marginLeft: "200px" }}>
+      <div className="text4 text4d">
         <div>
           <div className="cof">
             <img src="./images/cofeee.svg" className="" alt="ref" />
